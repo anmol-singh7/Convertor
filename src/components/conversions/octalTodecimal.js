c13d981cea14ef1be59abec30e7b22d1244cdba7
Anmol Singh <singhanmol0984@gmail.com>
2023-01-31 02:14:21 +0530

    add error function to initially forget and update their conversion function

--- src/components/conversions/octalTodecimal.js
+++ src/components/conversions/octalTodecimal.js
@@ -1,21 +1,20 @@
 import React from 'react';
 import Head from "../Head"
 function octalTodecimal() {
-    function octalToDecimal(octal) {
-        let decimal = 0;
-        for (let i = 0; i < octal.length; i++) {
-            let digit = parseInt(octal[i]);
-            if (isNaN(digit) || digit < 0 || digit > 7) {
-                return `Wrong input: ${octal[i]} is not a valid octal digit.`;
-            }
-            decimal = decimal * 8 + digit;
+    
+       const  octalToDecimal=(octal)=> {
+            return parseInt(octal, 8);
         }
-        return decimal;
-    }
 
+    const error = (str) => {
+        if (str.length !== 0) {
+            return /^[0-7]+$/.test(str) ? "" : `${str[str.length - 1]} is not a valid Octal symbol. Valid Octal symbols are 0-7`;
+        }
+        return "";
+    }
     return (
         <div className="container">
-            <Head resultMaker={octalToDecimal} />
+            <Head resultMaker={octalToDecimal} error={error} />
             octalTodecimal
         </div>
     )
--- src/components/conversions/octalTohexadecimal.js
+++ src/components/conversions/octalTohexadecimal.js
@@ -4,14 +4,18 @@ function octalTohexadecimal() {
 
     const octal_hexa = (string) => {
 
-        var x = Number(string);
-        return x.toString(16);
+            return parseInt(string, 8).toString(16);
     }
-
+    const error = (str) => {
+        if (str.length !== 0) {
+            return /^[0-7]+$/.test(str) ? "" : `${str[str.length - 1]} is not a valid Octal symbol. Valid Octal symbols are 0-7`;
+        }
+        return "";
+    }
 
     return (
         <div className="container">
-            <Head />
+            <Head resultMaker={octal_hexa} error={error} />
             octalTohexadecimal
         </div>
     )
